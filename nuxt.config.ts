8a2777ae03c10863f6cd34cd88fc1838d0b8f5bc
Rajkumaryuktrix <admin@yuktrix.in>
2026-02-25 08:49:33 +0530

    Update site content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/google-fonts', '@nuxtjs/tailwindcss'],
+  modules: ['@netlify/nuxt','@nuxtjs/google-fonts', '@nuxtjs/tailwindcss'],
   compatibilityDate: '2026-02-23',
   googleFonts: {
     families: {
